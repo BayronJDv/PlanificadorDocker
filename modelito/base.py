8565af461d3776972386949a39ad6bcd76220ad1
AMG <146374366+SARTDv@users.noreply.github.com>
2024-07-08 13:48:30 -0500

    Update base.py

--- modelito/base.py
+++ modelito/base.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 # intencion Gestionar la coneccion inicial a la base de datos
 
-data_base_url = 'postgresql://postgres:pescado@localhost/testso'
+data_base_url = 'postgresql://postgres:pescado@localhost/postgres'
 engine = create_engine(data_base_url)
 Session = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
